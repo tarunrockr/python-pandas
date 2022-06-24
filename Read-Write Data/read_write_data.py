e47580aa54735de5a3e44ef52c85e5a201c8a1b7
tarunrockr <tarun94060sharma@gmail.com>
2022-06-24 12:53:39 +0530

    Use of loc, iloc, at, iat in reading data, ways of adding columns to existing dataframe

--- Read-Write Data/read_write_data.py
+++ Read-Write Data/read_write_data.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 # ----------------- Reading csv file with header ------------------
 
-path = "../sample_files/data.csv"
+# path = "../sample_files/data.csv"
 # df = pd.read_csv(path)
 # df = pd.read_csv(path, skiprows=1) # skiprows is to skip first row
 # df = pd.read_csv(path, header=1) # header is pointing to second row
@@ -99,8 +99,174 @@ path = "../sample_files/data.csv"
 # 	customer_df.to_excel(writer, sheet_name="customer_data", index=False)
 
 
+# ----------------------- Pandas loc function to read data loc[rows, columns] ----------------------------------------------------------
+
+path = "../sample_files/sample_excel.xlsx"
+df = pd.read_excel(path)
+print("------------------ Main dataframe -------------------")
+print(df)
+print()
+
+# columnwise selection
+
+#  Reading all rows and one specific colunm
+print("-------- Reading all rows and one specific column using loc ------")
+print(df.loc[:,'Name' ])
+print()
+
+#  Reading all rows and some specific columns
+print("-------- Reading all rows and some specific columns using loc ------")
+print(df.loc[:,['Name', 'Address'] ])
+print()
+
+# rowwise selection
+
+#  Reading one row and all colunms
+print("-------- Reading one row and all colunms using loc ------")
+print(df.loc[ 3,: ])
+print()
+
+#  Reading some rows and all columns
+print("-------- Reading some rows and all columns using loc ------")
+print(df.loc[ [3,4], : ])
+print()
+
+# Specific cell selection
+
+#  Reading some rows and some columns
+print("-------- Reading some rows and some columns using loc ------")
+print(df.loc[ [3, 4], ['Name', 'Address'] ])
+print()
+
+#  Reading single cell value
+print("-------- Reading single cell value using loc ------")
+print(df.loc[3, 'Name'])
+print()
+
+# Reading single row
+print("-------- Reading single row using loc ------")
+print(df.loc[3])
+print()
 
 
+# ----------------------- Pandas iloc function to read data iloc[rows, columns] (We use index values instead of row and column names ---------------------------------------------
+
+path = "../sample_files/sample_excel.xlsx"
+df = pd.read_excel(path)
+print("------------------ Main dataframe -------------------")
+print(df)
+print()
+
+# columnwise selection
+
+# Reading all rows and one specific colunm
+print("-------- Reading all rows and one specific column using iloc ------")
+print(df.iloc[:, 0 ])
+print()
+
+#  Reading all rows and some specific columns
+print("-------- Reading all rows and some specific columns using iloc ------")
+print(df.iloc[:, [0, 1] ])
+print()
+
+# rowwise selection
+
+#  Reading one row and all colunms
+print("-------- Reading one row and all colunms using iloc ------")
+print(df.iloc[ 3,: ])
+print()
+
+#  Reading some rows and all columns
+print("-------- Reading some rows and all columns using iloc ------")
+print(df.iloc[ [3,4], : ])
+print()
+
+# Specific cell selection
+
+#  Reading some rows and some columns
+print("-------- Reading some rows and some columns using iloc ------")
+print(df.iloc[ [3, 4], [0, 1] ])
+print()
+
+#  Reading single cell value
+print("-------- Reading single cell value using iloc ------")
+print(df.iloc[3, 0])
+print()
+
+# Reading single row
+print("-------- Reading single row using iloc ------")
+print(df.iloc[3])
+print()
+
+# ----------------------- Pandas at function to read data at[rows, columns]  ---------------------------------------------
+
+path = "../sample_files/sample_excel.xlsx"
+df = pd.read_excel(path)
+print("------------------ Main dataframe -------------------")
+print(df)
+print()
+
+#  Reading single cell value
+print("-------- Reading single cell value using at ------")
+print(df.at[3, 'Name'])
+print()
+
+# ----------------------- Pandas at function to read data iat[rows, columns] (We use column indexes instead of column names)  ---------------------------------------------
+
+path = "../sample_files/sample_excel.xlsx"
+df = pd.read_excel(path)
+print("------------------ Main dataframe -------------------")
+print(df)
+print()
+
+#  Reading single cell value
+print("-------- Reading single cell value using iat ------")
+print(df.iat[3, 0])
+print()
+
+
+# ------------------ Adding columns to dataframe(4 ways of adding columns) ---------------
+
+print("------------  1st way of adding column to exising dataframe -----------------")
+df['Education'] = ['10 th', '12 th', 'B.A.', 'B.Sc.', 'B.Com.', 'B.Tech.']
+print(df)
+df.drop(['Education'], axis=1, inplace=True)
+print()
+
+print("------------  adding multiple columns at once to exising dataframe -----------------")
+df[ ['col1', 'col2', 'col3'] ] = np.random.randint(10, size=(6,3))
+print(df)
+df.drop([ 'col1', 'col2', 'col3' ], axis=1, inplace=True)
+print()
+
+print("------------  2nd way of adding column to exising dataframe using index(index, column_name, value) -----------------")
+df.insert(1, 'col1_ind', ['10 th', '12 th', 'B.A.', 'B.Sc.', 'B.Com.', 'B.Tech.'])
+print(df)
+df.drop(['col1_ind'], axis=1, inplace=True)
+print()
+
+df.insert(1, 'col1_ind', 5)
+print(df)
+df.drop(['col1_ind'], axis=1, inplace=True)
+print()
+
+df.insert( loc=1, column='col1_ind', value=5)
+print(df)
+df.drop(['col1_ind'], axis=1, inplace=True)
+print()
+
+
+print(("-----------  3rd way of adding column to existing dataframe using df.assign(new_column=lambda x: x.another_column * 3) "))
+df = df.assign(Roll_Number = lambda x: x.Age * 4 )
+print(df)
+df.drop(['Roll_Number'], axis=1, inplace=True)
+print()
+
+print(("-----------  4th way of adding column to existing dataframe using loc[:,'column_name'] = [val1, val2, ....] "))
+df.loc[:,"Roll_Number"] = [1,2,3,4,5,6]
+print(df)
+df.drop(['Roll_Number'], axis=1, inplace=True)
+print()
 
 
 
